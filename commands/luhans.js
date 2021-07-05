a4560746871812ac7b628cfa44a6e9150c516ba8
Sadeiko <xovval@gmail.com>
2021-07-05 10:46:33 +0200

    Fair update of luhans
    The full potential of luhans is still not revealed - this commit will spice up some features and maybe make the useage of luhans more fun. Overhaul is needed though.

--- commands/luhans.js
+++ commands/luhans.js
@@ -1,6 +1,14 @@
 const StatHandler = require("../helpers/stat-handler.js");
 
+
+//This needs to be improved -> general access
+function getRandomInt(end){
+    return Math.floor(Math.random() * end);
+};
+
+
 const medienKlausur = new Date('2021-07-02T11:00:00');
+
 const names = [
     'McLuhans',
     'McWuhans',
@@ -11,30 +19,100 @@ const names = [
     'McRamonihans',
     'Roman',
     'Jürgen',
-    'McJohans'
+    'McJohans',
+    'Emsiger Eder'
 ];
-       //Dein Mediendispositiv komme, sein Wille geschehe, wie im *name* so im heiligen Angry.
+
+//This needs to get into a json at some point
+const medienDispositive = [
+    'Medien sind Vermittlungsinstanz, die zum einen zwischen Sprecher und Hörer, Produzent und Rezipient, die zum anderen zwischen dem Menschen und seiner Umwelt, die wir abgekürzt “Realität” nennen, vermitteln und in der Regel diese Realität oder Aspekte von ihr in einem Medienprodukt darstellen.',
+    'Medien sind nicht nur „mimetisch“(Nachahmend), „reproduzierend“ oder „abbildend“, sondern sie erzeugen selbst eine eigene Realität. Wir nennen in Abgrenzung zur medialen “Realität” diese Welt und ihre Strukturen „Wirklichkeit“ und gehen davon aus, dass die Medien nicht nur insgesamt sondern auch jeweils als einzelnes Medium Medienwirklichkeiten entstehen lassen. Dabei geht es vor allem darum, dass die Medienwirklichkeit für den Menschen als Realität konstitutiv wird',
+    'Aura: einmalige Erscheinung einer Ferne, so nah sie sein mag',
+    '„Die Zertrümmerung der Aura ist die Signatur einer Wahrnehmung, deren Sinn für alles Gleichartige auf der Welt so gewachsen ist, dass sie es mittels der Reproduktion auch dem Einmaligen abgewinnt.”',
+    'Aura wird durch Reproduktion zertrümmert, weil sie nur durch etwas Einzigartiges entstehen kann',
+    '„Der Kameramann dringt in sein Motiv ein, wie ein Chirurg in den Körper eines Patienten.“',
+    '„Lust am Schauen und Erleben“',
+    '„In den Kirchen und Klöstern des Mittelalters und an den Fürstenhöfen bis gegen Ende des achtzehnten Jahrhunderts, fand die allgemeine Kollektivrezeption von Gemälden nicht simultan, sondern vielfach gestuft und hierarchisch vermittelt statt.“',
+    'Apperzeption ist die Aneignung eines Gegenstands durch das Zusammenspiel von sinnlicher und geistiger Wahrnehmung.',
+    'Medien sind Erweiterungen des menschlichen Körpers.',
+    '„Inhalt“ jedes Mediums immer ein anderes Medium.',
+    '„Denn die Botschaft jedes Mediums oder jeder Technik ist die Veränderung des Maßstabs, Tempos oder Schemas, die es der Situation des Menschen bringen”',
+    'Medien greifen Ausdrucks- und Darstellungsformen andere Medien auf, wenn diese sich bewährt haben oder kulturell attraktiv sind.',
+    'Heißes Medium verlangt weniger Beteiligung als ein kaltes.',
+    '*Auto*\nA. Dehnt die Privatsphäre aus, Menschen fahren in ihren Autos hinaus, um alleine zu sein.\nB. Veraltet Pferd und Wagen, Kombiwagen.\nC. Lässt die Sehnsucht nach der Suche wiederkehren: wie ein Ritter in glänzender Rüstung.\nD. Bis zu ihren Grenzen getrieben, verwandelt das Auto die Stadt in die AußenStadt oder VorStadt. Bringt das Gehen als Kunstform zurück.',
+    'Dispositive steuern unsere Wahrnehmung (die Art und Weise), wie wir die Welt wahrnehmen.',
+    'Dispositive sind Anordnungen unterschiedlicher Art, die regeln, wie die Menschen innerhalb einer Kultur etwas wahrnehmen, die Sichtbarkeit erzeugen, ohne selbst sichtbar zu sein.',
+    'Mediendispositiv: Zusammenwirken verschiedener Ebenen, auf denen sich gesellschaftliche Normen und Werte formulieren, sich durchsetzen und damit Macht ausüben.',
+    'Bevor das Kino die Erfüllung technischer Voraussetzungen und eines bestimmten Gesellschaftszustandes war (die für seine Realisierung und seine Vorstellung notwendig war), mag es zunächst das Ziel eines Wunsches gewesen sein, den übrigens sowohl sein unmittelbarer Erfolg als auch das von seinen Vorfahren geweckte Interesse hinlänglich zum Ausdruck brachten. Ein Wunsch, sagen wir mit Bedacht, eine Form von verloren gegangener Befriedigung, die auf die eine oder andere Form wiederzuerlangen das Ziel seines Dispositivs ist (bis hin zu ihrer Simulation) und zu welcher der Realitätseindruck den Schlüssel zu liefern scheint.',
+    'Mediendispositive konstruieren eine größere Ordnung, in der mediale Kommunikation zu sehen ist, primär vom Spannungsverhältnis “Technik-Subjekt” her. Sie gehen vom Einzelnen und seiner Rezeption aus und untersuchen aus der Perspektive des Subjekts, wie macht Instanzen auf die mediale Wahrnehmung des Einzelnen einwirken. Es ist in diesem Sinne ein medienkritisches Konzept, das versucht, die unbewussten und verborgenen Mechanismen der Medienkommunikation sichtbar zu machen und damit auf inhärente Beeinflussunsstrukturen hinzuweisen.'
+];
+
+const geschmacksliste = [
+    'Wut der Faulheit',
+    'Wut der Trockenheit',
+    'Wut der Fledermaus',
+    'Wut des Klebers',
+    'Wut der Unwissenheit',
+    'Wut des Johans'
+];
+
+const funStuff = [
+    `Ich bin ${names[getRandomInt(names.length)]}, ich sehe ohne gesehen zu werden.`,
+    `Ich bin ${names[getRandomInt(names.length)]}, ich laufe rund und mich gibt's in vielen, verschiedenen Geschmäckern - ich bin ein Mediendispositiv in der ${geschmacksliste[getRandomInt(geschmacksliste.length)]}!`,
+    `Ich bin ${names[getRandomInt(names.length)]}, ich sehe ohne gesehen zu werden.`,
+    `Heute bist du ein ${names[getRandomInt(names.length)]}. Sei glücklich und feiere wie ein ${names[getRandomInt(names.length)]}!`,
+    `Ich bin ${names[getRandomInt(names.length)]}, ich sehe ohne gesehen zu werden.`,
+    `Dein Mediendispositiv komme, ${names[getRandomInt(names.length)]}s Wille geschehe, wie im HS4 so im heiligen Angry.`,
+    `Spüre die Wut der ${geschmacksliste[getRandomInt(geschmacksliste.length)]}!`,
+];
+
 module.exports = {
 	name: 'luhans',
 	description: 'Get the time since the medien-t test',
     hidden: true,
 	execute(msg) {
-        // Get a random name from the McLuhan name array
-        const random = Math.floor(Math.random() * names.length);
 
-        // Calculate the time since the medien-t test.
-        let msSinceKlausur = Date.now() - medienKlausur.getTime();
-        const dSinceKlausur = Math.floor(msSinceKlausur/1000/60/60/24);
-        msSinceKlausur -= dSinceKlausur*1000*60*60*24;
-        
-        const hSinceKlausur = Math.floor(msSinceKlausur/1000/60/60);
-        msSinceKlausur -= hSinceKlausur*1000*60*60;
-        
-        const mSinceKlausur = Math.floor(msSinceKlausur/1000/60);
-        msSinceKlausur -= mSinceKlausur*1000*60;
-        
-        const sSinceKlausur = Math.floor(msSinceKlausur/1000);
+        // This will store the text which the bot will send
+        let returnText;
 
-        msg.channel.send(`${dSinceKlausur} Tage ${hSinceKlausur} Stunden ${mSinceKlausur} Minuten und ${sSinceKlausur} Sekunden sind seit der Medientheorie Klausur mit ${names[random]} vergangen.\nIch hoffe das macht dich wütend.`);
+        switch (getRandomInt(3)){
+
+            // Case to get fun stuff
+            case 0: returnText = funStuff[getRandomInt(funStuff.length)];
+            break;
+
+            // Case to get some proper medienDispositive!
+            case 1: returnText = medienDispositive[getRandomInt(medienDispositive.length)];
+            break;
+
+            // Good to know when this exam ended
+            case 2:
+                // Calculate the time since the medien-t test.
+                let msSinceKlausur = Date.now() - medienKlausur.getTime();
+                const dSinceKlausur = Math.floor(msSinceKlausur/1000/60/60/24);
+                msSinceKlausur -= dSinceKlausur*1000*60*60*24;
+                
+                const hSinceKlausur = Math.floor(msSinceKlausur/1000/60/60);
+                msSinceKlausur -= hSinceKlausur*1000*60*60;
+                
+                const mSinceKlausur = Math.floor(msSinceKlausur/1000/60);
+                msSinceKlausur -= mSinceKlausur*1000*60;
+                
+                const sSinceKlausur = Math.floor(msSinceKlausur/1000);
+
+                // Setting the variables for the dynamic time description (difference between singular and plural)
+                let hourText, minuteText, secondText; 
+
+                // No need to calculate dayText since it has been days when this code was created!
+                sSinceKlausur > 1 ? secondText = "Sekunden" : secondText = "Sekunde";
+                mSinceKlausur > 1 ? minuteText = "Minuten" : minuteText = "Minute";
+                hSinceKlausur > 1 ? hourText = "Stunden" : hourText = "Stunde";
+
+                returnText = `Sei glücklich, es sind bereits ${dSinceKlausur} Tage ${hSinceKlausur} ${hourText} ${mSinceKlausur} ${minuteText} und ${sSinceKlausur} ${secondText} sind seit der Medientheorie Klausur mit ${names[getRandomInt(names.length)]} vergangen!\nIch hoffe, das macht dich wütend.`;
+            break;
+        }
+
+
+        msg.channel.send(returnText);
 	},
 };

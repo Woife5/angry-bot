1febc8b9a2bfe1c4b053759cb4fba83ef5b14638
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2021-07-16 17:49:41 +0200

    added a new helper function for date calculation

--- helpers/helper-functions.js
+++ helpers/helper-functions.js
@@ -10,4 +10,25 @@ module.exports = {
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min +1)) + min;
     },
+
+    /**
+     * Return the time in days, hours, minutes and seconds between two timestamps
+     * @param {Number} start Starting timestamp
+     * @param {Number} end Ending timestamp
+     * @returns {Object} An object containing the time difference between the two timestamps
+     * @example { days: 2, hours: 3, minutes: 8, seconds: 20 }
+     */
+    getTimeDiff(start, end) {
+        const diff = end - start;
+        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+        return {
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds
+        };
+    },
 };

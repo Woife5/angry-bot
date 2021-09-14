60a34fa74f9c98413f07a662d106417b27a3dd93
Wolfgang Schwendtbauer <wolfgang.schwendtbauer@gmail.com>
2021-09-14 20:40:57 +0200

    updated a command

--- commands/irmi.js
+++ commands/irmi.js
@@ -5,5 +5,8 @@ module.exports = {
 	execute(msg) {
         // msg.channel.send('I\'m not doing anything');
         // no seriously, this is not doing anthing.
+
+		// okay maybe react with a sleepy emoji
+		msg.react('😴');
 	},
 };
